27ad205487510dedd819f23cb765ed915ed01369
ddl <ddl@novatec-gmbh.de>
2022-02-12 10:55:49 +0100

    tests: Commands functions to seedAndVisit

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,10 +1,8 @@
 describe('App initialization', () => {
     it.only('Loads todos on page load', () => {
-      cy.server();
-      cy.route('GET', '/api/todos', 'fixture:todos');
-      cy.visit('/');
+        cy.seedAndVisit();
 
-      cy.get('.todo-list li')
+        cy.get('.todo-list li')
           .should('have.length', 4);
 
     });
