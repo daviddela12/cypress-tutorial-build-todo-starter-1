bc2ecb34482dcb9c36f41cbd2402f9b5e8df2303
ddl <ddl@novatec-gmbh.de>
2022-02-15 17:55:41 +0100

    test: Footer display correct count when display more than one elements

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -1,9 +1,18 @@
 describe("Footer", () => {
     context("with a single todo", () => {
-        it.only("display a singular todo in count",() => {
+        it("display a singular todo in count",() => {
             cy.seedAndVisit([{id: 1, name: "Buy chocolate", isComplete: false}]);
             cy.get('.todo-count')
-              .should("contain", 1)
+              .should("contain", "1 todo left")
+        });
+
+        it("display two singular todos in count", () => {
+            cy.seedAndVisit([
+                {id: 1, name: "Buy chocolate", isComplete: false},
+                {id: 2, name: "Buy bread", isComplete: false}
+            ]);
+            cy.get('.todo-count')
+              .should('contain', '2 todos left');
         });
     });
 });
