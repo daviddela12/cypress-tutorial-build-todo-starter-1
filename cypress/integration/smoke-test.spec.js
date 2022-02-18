22dd22a590dafe7d5381e20ff91dcd6b76029a25
ddl <ddl@novatec-gmbh.de>
2022-02-18 10:27:56 +0100

    refact: Wrap to try adding more than one item in todo list

--- cypress/integration/smoke-test.spec.js
+++ cypress/integration/smoke-test.spec.js
@@ -6,19 +6,28 @@ describe("The application loads", () => {
         .its('body')
         .each(todo => cy.request('DELETE', `/api/todos/${todo.id}`))
     })
-    it("Add new item", () => {
+    it("Add new todo items", () => {
+      const items = [
+        {todo: 'Buy milk', expectedLength: 1},
+        {todo: 'Buy eggs', expectedLength: 2},
+        {todo: 'Buy chocolate', expectedLength: 3}
+      ]
       cy.visit('/')
       cy.server()
       cy.route('POST', '/api/todos')
         .as('create')
 
-      cy.get('.new-todo')
-        .type('Buy milk{enter}')
-      
-      cy.wait('@create')
-
-      cy.get('.todo-list li')
-        .should('have.length', 1)
+      cy.wrap(items)
+        .each(item => {
+          cy.get('.new-todo')
+            .type(item.todo)
+            .type('{enter}')
+          
+          cy.wait('@create')
+    
+          cy.get('.todo-list li')
+            .should('have.length', item.expectedLength)
+        })
     })
   })
 });
